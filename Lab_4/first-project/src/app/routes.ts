6d406c4543597176a872c4e0b5b9ed1c54154d6f
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2025-02-16 00:51:33 +0500

    test

--- Lab_4/first-project/src/app/routes.ts
+++ Lab_4/first-project/src/app/routes.ts
@@ -4,14 +4,14 @@ import { DetailsComponent } from "./details/details.component";
 
 
 export const routeConfig: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      title: 'Home page',
-    },
-    {
-      path: 'details/:id',
-      component: DetailsComponent,
-      title: 'Home details',
-    },
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Home details',
+  },
 ];
